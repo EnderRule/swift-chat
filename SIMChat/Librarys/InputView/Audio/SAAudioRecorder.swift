2b18e188f6e6d2f29532da3f7afd5a7f36ff5d5c
sagesse <gdmmyzc@163.com>
2016-09-17 18:08:31 +0800

    add audio recorder for audio input view

---
+++ SIMChat/Librarys/InputView/Audio/SAAudioRecorder.swift
@@ -0,0 +1,13 @@
+//
+//  SAAudioRecorder.swift
+//  SIMChat
+//
+//  Created by sagesse on 9/17/16.
+//  Copyright © 2016 sagesse. All rights reserved.
+//
+
+import UIKit
+
+class SAAudioRecorder: NSObject {
+
+}
