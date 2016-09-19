7ffd78e1cfcaccbc21bfa9889f182a13563bab1c
sagesse <gdmmyzc@163.com>
2016-09-19 09:21:21 +0800

    add mask view of audio input view
     Changes to be committed:
    	modified:   SIMChat/Librarys/InputView/Audio/SAAudioInputView.swift
    	modified:   SIMChatExample.xcodeproj/project.xcworkspace/xcuserdata/sagesse.xcuserdatad/UserInterfaceState.xcuserstate

--- SIMChat/Librarys/InputView/Audio/SAAudioInputView.swift
+++ SIMChat/Librarys/InputView/Audio/SAAudioInputView.swift
@@ -38,6 +38,12 @@ public protocol SAAudioInputViewDataSource: NSObjectProtocol {
 public protocol SAAudioInputViewDelegate: NSObjectProtocol {
     
     @objc optional func inputViewContentSize(_ inputView: UIView) -> CGSize
+    
+    @objc optional func audio(_ audio: SAAudioInputView, shouldStartRecord url: URL) -> Bool
+    @objc optional func audio(_ audio: SAAudioInputView, didStartRecord url: URL)
+    
+    @objc optional func audio(_ audio: SAAudioInputView, didRecordComplete url: URL, duration: TimeInterval)
+    @objc optional func audio(_ audio: SAAudioInputView, didRecordFailure url: URL, duration: TimeInterval)
 }
 
 
@@ -80,6 +86,9 @@ open class SAAudioInputView: UIView {
         
         backgroundColor = UIColor(colorLiteralRed: 0xec / 0xff, green: 0xed / 0xff, blue: 0xf1 / 0xff, alpha: 1)
         
+        _maskView.backgroundColor = UIColor(white: 0, alpha: 0.2)
+        _maskView.translatesAutoresizingMaskIntoConstraints = false
+        
         _contentView.backgroundColor = .clear
         _contentView.isPagingEnabled = true
         _contentView.showsVerticalScrollIndicator = false
@@ -113,6 +122,9 @@ open class SAAudioInputView: UIView {
     fileprivate lazy var _contentViewLayout: SAAudioInputViewLayout = SAAudioInputViewLayout()
     fileprivate lazy var _contentView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: self._contentViewLayout)
     
+    fileprivate lazy var _maskView: UIView = UIView()
+    fileprivate lazy var _maskViewLayout: [NSLayoutConstraint] = []
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
         _init()
@@ -144,27 +156,70 @@ extension SAAudioInputView: UICollectionViewDataSource, UICollectionViewDelegate
     }
 }
 
-// MARK: - SAAudioViewDelegate
+// MARK: - SAAudioViewDelegate(Forwarding)
 
 extension SAAudioInputView: SAAudioViewDelegate {
     
     func audioView(_ audioView: SAAudioView, shouldStartRecord url: URL) -> Bool {
-        _logger.trace()
-        return true
+        return delegate?.audio?(self, shouldStartRecord: url) ?? true
     }
     func audioView(_ audioView: SAAudioView, didStartRecord url: URL) {
-        _logger.trace()
-        
-        _contentView.isScrollEnabled = false
+        addMaskView()
+        delegate?.audio?(self, didStartRecord: url)
     }
     
     func audioView(_ audioView: SAAudioView, didComplete url: URL, duration: TimeInterval) {
-        _logger.trace()
-        _contentView.isScrollEnabled = true
+        removeMaskView()
+        delegate?.audio?(self, didRecordComplete: url, duration: duration)
     }
     func audioView(_ audioView: SAAudioView, didFailure url: URL, duration: TimeInterval) {
+        removeMaskView()
+        delegate?.audio?(self, didRecordFailure: url, duration: duration)
+    }
+    
+    func addMaskView() {
+        guard let window = window else {
+            return
+        }
+        guard _maskView.superview == nil else {
+            return
+        }
+        
         _logger.trace()
         
+
+        window.addSubview(_maskView)
+        _maskViewLayout = [
+            _SALayoutConstraintMake(_maskView, .top, .equal, window, .top),
+            _SALayoutConstraintMake(_maskView, .left, .equal, window, .left),
+            _SALayoutConstraintMake(_maskView, .right, .equal, window, .right),
+            _SALayoutConstraintMake(_maskView, .bottom, .equal, self, .top),
+        ]
+        window.addConstraints(_maskViewLayout)
+        
+        self._maskView.alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self._maskView.alpha = 1
+        }
+        
+        _contentView.isScrollEnabled = false
+        _contentView.panGestureRecognizer.isEnabled = false
+    }
+    func removeMaskView() {
+        guard _maskView.superview != nil else {
+            return
+        }
+        _logger.trace()
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self._maskView.alpha = 0
+        }, completion: { _ in
+            self._maskView.superview?.removeConstraints(self._maskViewLayout)
+            self._maskView.removeFromSuperview()
+            self._maskViewLayout = []
+        })
+        
         _contentView.isScrollEnabled = true
+        _contentView.panGestureRecognizer.isEnabled = true
     }
 }
