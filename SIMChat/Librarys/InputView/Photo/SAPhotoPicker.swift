57852f9453a83f18f58bd88305fc81d9329b5dd9
sagesse <gdmmyzc@163.com>
2016-09-27 18:41:58 +0800

    redesign photo picker for photo input view
    Changes to be committed:
      modified:   SIMChat/Librarys/InputView/Photo/SAPhotoPicker.swift
      modified:   SIMChat/Librarys/InputView/Photo/SAPhotoPickerAlbums.swift

--- SIMChat/Librarys/InputView/Photo/SAPhotoPicker.swift
+++ SIMChat/Librarys/InputView/Photo/SAPhotoPicker.swift
@@ -35,10 +35,6 @@ open class SAPhotoPicker: UINavigationController {
     
     open var allowsMultipleSelection: Bool = true
     
-    open override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
-        super.dismiss(animated: flag, completion: completion)
-        _delegate?.picker?(didDismiss: self)
-    }
     
     open override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,83 +43,29 @@ open class SAPhotoPicker: UINavigationController {
         navigationItem.rightBarButtonItem = _cancelBarItem
     }
     
-    open override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if !_isInitAlbums {
-            _isInitAlbums = true
-            _loadAlbums()
-        }
+    open override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        super.dismiss(animated: flag, completion: completion)
+        _delegate?.picker?(didDismiss: self)
     }
     
-    func onCancel(_ sender: Any) {
+    @objc private func cancelHandler(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-//        onDidDismiss(self)
-    }
-    
-    private func _showErrorView() {
-        _logger.trace()
-        
-        let vc = SAPhotoPickerError(image: UIImage(named: "photo_error_permission"), title: nil)
-        vc.navigationItem.rightBarButtonItem = _cancelBarItem
-        viewControllers = [vc]
-    }
-    private func _showEmptyView() {
-        _logger.trace()
-        
-        let vc = SAPhotoPickerError(image: UIImage(named: "photo_error_empty"), title: nil)
-        vc.navigationItem.rightBarButtonItem = _cancelBarItem
-        viewControllers = [vc]
-    }
-    private func _showContentView() {
-        _logger.trace()
-        
-        let vc = _albumnsViewController
-        vc.navigationItem.rightBarButtonItem = _cancelBarItem
-        vc.reloadData(with: _albums)
-        viewControllers = [vc]
-    }
-    
-    fileprivate func _reloadAlbums(_ hasPermission: Bool) {
-        guard hasPermission else {
-            _showErrorView()
-            return
-        }
-        _albums = SAPhotoAlbum.albums.filter {
-            !$0.photos.isEmpty
-        }
-        guard let albums = _albums, !albums.isEmpty else {
-            _showEmptyView()
-            return
-        }
-        _showContentView()
-    }
-    fileprivate func _loadAlbums() {
-        SAPhotoLibrary.shared.requestAuthorization {
-            self._reloadAlbums($0)
-        }
     }
     
     private func _init() {
         
-        _albumnsViewController.picker = self
-        _albumnsViewController.toolbarItems = toolbarItems
+        _rootViewController.picker = self
+        _rootViewController.toolbarItems = toolbarItems
+        _rootViewController.navigationItem.rightBarButtonItem = _cancelBarItem
         
-        SAPhotoLibrary.shared.register(self)
+        // :)
+        self.viewControllers = [_rootViewController]
     }
     private func _deinit() {
         
         SAPhotoAlbum.reloadData()
-        SAPhotoLibrary.shared.unregisterChangeObserver(self)
     }
     
-    private var _isInitAlbums: Bool = false
-    private var _isFirstLoad: Bool = false
-    
-    private var _albums: [SAPhotoAlbum]?
-    
-    private lazy var _cancelBarItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(onCancel(_:)))
-    private lazy var _albumnsViewController: SAPhotoPickerAlbums = SAPhotoPickerAlbums()
     
     fileprivate weak var _delegate: SAPhotoPickerDelegate? {
         return delegate as? SAPhotoPickerDelegate
@@ -132,12 +74,17 @@ open class SAPhotoPicker: UINavigationController {
     fileprivate lazy var _selectedPhotos: Array<SAPhoto> = []
     fileprivate lazy var _selectedPhotoSets: Set<SAPhoto> = []
     
+    private lazy var _cancelBarItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelHandler(_:)))
+    private lazy var _rootViewController: SAPhotoPickerAlbums = SAPhotoPickerAlbums()
+    
+    
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         _init()
     }
     public override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
+        _init()
     }
     public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -157,15 +104,15 @@ open class SAPhotoPicker: UINavigationController {
 extension SAPhotoPicker: SAPhotoSelectionable {
     
     /// gets the index of the selected item, if item does not select to return NSNotFound
-    public func selection(_ selection: Any, indexOfSelectedItemsFor photo: SAPhoto) -> Int {
+    open func selection(_ selection: Any, indexOfSelectedItemsFor photo: SAPhoto) -> Int {
         return _delegate?.picker?(self, indexOfSelectedItemsFor: photo) ?? _selectedPhotos.index(of: photo) ?? NSNotFound
     }
    
     // check whether item can select
-    public func selection(_ selection: Any, shouldSelectItemFor photo: SAPhoto) -> Bool {
+    open func selection(_ selection: Any, shouldSelectItemFor photo: SAPhoto) -> Bool {
         return _delegate?.picker?(self, shouldSelectItemFor: photo) ?? true
     }
-    public func selection(_ selection: Any, didSelectItemFor photo: SAPhoto) {
+    open func selection(_ selection: Any, didSelectItemFor photo: SAPhoto) {
         if !_selectedPhotoSets.contains(photo) {
             _selectedPhotoSets.insert(photo)
             _selectedPhotos.append(photo)
@@ -174,10 +121,10 @@ extension SAPhotoPicker: SAPhotoSelectionable {
     }
     
     // check whether item can deselect
-    public func selection(_ selection: Any, shouldDeselectItemFor photo: SAPhoto) -> Bool {
+    open func selection(_ selection: Any, shouldDeselectItemFor photo: SAPhoto) -> Bool {
         return _delegate?.picker?(self, shouldDeselectItemFor: photo) ?? true
     }
-    public func selection(_ selection: Any, didDeselectItemFor photo: SAPhoto) {
+    open func selection(_ selection: Any, didDeselectItemFor photo: SAPhoto) {
         if let index = _selectedPhotos.index(of: photo) {
             _selectedPhotoSets.remove(photo)
             _selectedPhotos.remove(at: index)
@@ -186,17 +133,7 @@ extension SAPhotoPicker: SAPhotoSelectionable {
     }
     
     // tap item
-    public func selection(_ selection: Any, tapItemFor photo: SAPhoto) {
+    open func selection(_ selection: Any, tapItemFor photo: SAPhoto) {
         _delegate?.picker?(self, tapItemFor: photo, with: selection)
-    }
-}
-
-// MARK: - PHPhotoLibraryChangeObserver
-
-extension SAPhotoPicker: PHPhotoLibraryChangeObserver {
-    public func photoLibraryDidChange(_ changeInstance: PHChange) {
-        DispatchQueue.main.async {
-            self._reloadAlbums(true)
-        }
     }
 }
--- SIMChat/Librarys/InputView/Photo/SAPhotoPickerAlbums.swift
+++ SIMChat/Librarys/InputView/Photo/SAPhotoPickerAlbums.swift
@@ -46,11 +46,34 @@ internal class SAPhotoPickerAlbums: UITableViewController {
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = .none
         tableView.register(SAPhotoPickerAlbumsCell.self, forCellReuseIdentifier: "Item")
+        tableView.isScrollEnabled = false
+        
+        let tip = UIView()
+        
+        tip.backgroundColor = .purple
+//        tip.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tip)
+//        view.addConstraints([
+//            _SALayoutConstraintMake(tip, .top, .equal, topLayoutGuide, .bottom),
+//            _SALayoutConstraintMake(tip, .left, .equal, view, .left),
+//            _SALayoutConstraintMake(tip, .right, .equal, view, .right),
+//            _SALayoutConstraintMake(tip, .height, .equal, view, .height),
+//        ])
+        self.tip = tip
     }
+    
+    var tip: UIView?
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         navigationController?.isToolbarHidden = true
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        self.tip?.frame = view.convert(view.frame, from: view.window)
     }
 
     // MARK: - Table view data source
@@ -82,5 +105,66 @@ internal class SAPhotoPickerAlbums: UITableViewController {
         show(picker, sender: indexPath)
     }
     
+//    private var _isInitAlbums: Bool = false
+//    private var _isFirstLoad: Bool = false
+//    
+//    private var _albums: [SAPhotoAlbum]?
+    
     private var _albums: [SAPhotoAlbum]?
 }
+
+//// MARK: - PHPhotoLibraryChangeObserver
+//
+//extension SAPhotoPicker: PHPhotoLibraryChangeObserver {
+//    
+//    public func photoLibraryDidChange(_ changeInstance: PHChange) {
+//        _logger.trace()
+////        DispatchQueue.main.async {
+////            self._reloadAlbums(true)
+////        }
+//    }
+//}
+
+//    private func _showErrorView() {
+//        _logger.trace()
+//        
+//        let vc = SAPhotoPickerError(image: UIImage(named: "photo_error_permission"), title: nil)
+//        vc.navigationItem.rightBarButtonItem = _cancelBarItem
+//        viewControllers = [vc]
+//    }
+//    private func _showEmptyView() {
+//        _logger.trace()
+//        
+//        let vc = SAPhotoPickerError(image: UIImage(named: "photo_error_empty"), title: nil)
+//        vc.navigationItem.rightBarButtonItem = _cancelBarItem
+//        viewControllers = [vc]
+//    }
+//    private func _showContentView() {
+//        _logger.trace()
+//        
+//        let vc = _albumnsViewController
+//        vc.navigationItem.rightBarButtonItem = _cancelBarItem
+//        vc.reloadData(with: _albums)
+//        viewControllers = [vc]
+//    }
+//    
+//    fileprivate func _reloadAlbums(_ hasPermission: Bool) {
+//        guard hasPermission else {
+//            _showErrorView()
+//            return
+//        }
+//        _albums = SAPhotoAlbum.albums.filter {
+//            !$0.photos.isEmpty
+//        }
+//        guard let albums = _albums, !albums.isEmpty else {
+//            _showEmptyView()
+//            return
+//        }
+//        _showContentView()
+//    }
+//    fileprivate func _loadAlbums() {
+//        SAPhotoLibrary.shared.requestAuthorization {
+//            self._reloadAlbums($0)
+//        }
+//    }
+    
